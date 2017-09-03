c84a7c4171587c30947faa50a057089046ae0c2f
Shubham Sharma <mr.shubhshar@gmail.com>
2017-09-03 23:42:49 +0530

    working (dummy history)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
@@ -9,6 +9,11 @@ socketio = SocketIO(app)
 def handleMessage(msg):
     print('Message: ' + msg)
     send(msg, broadcast=True)
+
+@app.route('/')
+def index():
+    messages = ['First message', 'Second message', 'Third message']
+    return render_template('index.html', messages=messages)
 
 if __name__ == '__main__':
     socketio.run(app)
